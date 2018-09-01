7dfe2008029a3c982084514510bb59b42fa882d5
Serina Ko <serinako08@gmail.com>
2018-09-01 12:33:19 +1000

    map through the data array and display some elements

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
-// import Brand from './Brand';
+import Brand from './Brand';
 import './App.css';
 
 class App extends Component {
   render() {
-    return <p>hello</p>;
+    return (
+      <div className="wrapper">
+        <div>
+          <Brand endpoint={'new_tokens'} title={'New Tokens'} />
+        </div>
+        <div>
+          <Brand endpoint={'popular_tokens'} title={'Popular Tokens'} />
+        </div>
+        <div>
+          <Brand endpoint={'popular_tokens'} title={'Popular Tokens'} />
+        </div>
+      </div>
+    );
   }
 }
 
--- src/components/Brand.js
+++ src/components/Brand.js
@@ -1,6 +1,55 @@
 import React from 'react';
-// import axios from 'axios';
+import axios from 'axios';
 
-class Brand extends React.Component {}
+class Brand extends React.Component {
+  state = {
+    brands: null
+  };
 
+  componentDidMount() {
+    const url =
+      'https://elbaite-staging.herokuapp.com/api/v1/main/' +
+      this.props.endpoint;
+    axios.get(url).then((res) => {
+      this.setState({
+        brands: res.data[this.props.endpoint]
+      });
+      console.log(res);
+    });
+  }
+
+  render() {
+    const { brands } = this.state;
+
+    if (brands !== null) {
+      const brandInfo = brands.map((brandData) => {
+        return (
+          <React.Fragment>
+            <div className="brand-data">
+              <div>
+                <p className="brand-name">{brandData.name}</p>
+                <p>{brandData.total_volume}</p>
+              </div>
+              <div>
+                <img src={brandData.icon_url} alt={true} />
+              </div>
+            </div>
+          </React.Fragment>
+        );
+      });
+      return (
+        <React.Fragment>
+          <h1>{this.props.title}</h1>
+          <div>{brandInfo}</div>
+        </React.Fragment>
+      );
+    } else {
+      return (
+        <div>
+          <p>Nothing to show</p>
+        </div>
+      );
+    }
+  }
+}
 export default Brand;
