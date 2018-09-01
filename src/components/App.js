c45efbf7ea843b16b2ae3d896744ef964178c41d
Serina Ko <serinako08@gmail.com>
2018-09-01 12:27:42 +1000

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// import Brand from './Brand';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return <p>hello</p>;
+  }
+}
+
+export default App;
---
+++ src/components/Brand.js
@@ -0,0 +1,6 @@
+import React from 'react';
+// import axios from 'axios';
+
+class Brand extends React.Component {}
+
+export default Brand;
